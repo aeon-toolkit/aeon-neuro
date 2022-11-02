bbd11e8ac5ac81412a7b4be739a0ce879915c03c
AidenRushbrooke <72034940+AidenRushbrooke@users.noreply.github.com>
2022-11-02 14:50:24 +0000

    added option to keep non-EEG channels

--- sktime-neuro/utils/data_io.py
+++ sktime-neuro/utils/data_io.py
@@ -1,24 +1,25 @@
 import numpy as np
 import mne
 
-def load_brainvision_to_numpy(path):
+def load_brainvision_to_numpy(path,remove_non_EEG=True):
     """
     Load EEG data from brainvision format into numpy array, removing non-EEG channels
 
     Parameters
     _____________
     path : the file path to the vhdr file
+    remove_non_EEG : if non-EEG channels should be removed prior to returning
+                     Default: True
 
     Returns
     _____________
     numpy_data: numpy.array
-        a numpy array containing the raw EEG data in the form num_channels*num_timepoints
+        a numpy array containing the raw EEG data in the form [num_channels][num_timepoints]
 
 
     """
-    raw_data = mne.io.read_raw_brainvision(path)
-    selected_EEG = raw_data.pick_types(eeg=True)
-    data = selected_EEG.load_data()
+    mne_data = mne.io.read_raw_brainvision(path)
+    if remove_non_EEG:
+        mne_data = mne_data.pick_types(eeg=True)
+    data = mne_data.load_data()
     return np.asarray(data.get_data())
-    
-
