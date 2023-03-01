eeda69e99c98ec53a52cfb6e2a2e79fc882c334b
AidenRushbrooke <72034940+AidenRushbrooke@users.noreply.github.com>
2023-03-01 23:44:15 +0000

    Bandpower Ensemble initial version

--- tsmlpy_neuro/transformations/bandpower.py
+++ tsmlpy_neuro/transformations/bandpower.py
@@ -3,35 +3,28 @@ import numpy as np
 from scipy.fft import rfft,rfftfreq
 from scipy.integrate import simps
 
-from sktime.transformations.base import BaseTransformer
 
-
-class BandpowerExtraction(BaseTransformer):
+class BandpowerExtraction():
     def __init__(self,fs,band,window_width=1000,window_space=5):
         self.fs = fs
         self.band = band
         self.window_width = window_width
         self.window_space = window_space
     
-    def _transform(self,X,y=None):
+    def transform(self,X,y=None):
         max_freq,min_freq = selectBandFreqs(self.band)
-
         n_instances,n_channels,n_timepoints = np.shape(X)
-        final_data = np.zeros((n_instances,n_channels,int(n_timepoints/self.window_spacing)+1))
-
+        final_data = np.zeros((n_instances,n_channels,int((n_timepoints-self.window_width)/self.window_space)+1))
         for instance in range(n_instances):
             for channel in range(n_channels):
-                padded_data=np.zeros(n_timepoints+self.window_size)
-                padded_data[int(self.window_size/2):n_timepoints+int(self.window_size/2)]=X[instance][channel]
-                for timepoint in range(1,n_timepoints+1,self.window_spacing):
-                    w = padded_data[timepoint:timepoint+self.window_size]
+                for timepoint in range(1,n_timepoints-self.window_width,self.window_space):
+                    w = X[instance][channel][timepoint:timepoint+self.window_width]
                     coefficients = rfft(w)[1:]
-                    freqs = rfftfreq(self.window_size,1/self.fs)[1:]
+                    freqs = rfftfreq(self.window_width,1/self.fs)[1:]
                     delta = np.logical_and(freqs>=min_freq,freqs<=max_freq)
                     powers = np.abs(coefficients[delta])
                     value = simps(powers,dx=freqs[1]-freqs[0])
-                    final_data[instance][channel][int(timepoint/self.window_spacing)]=value
-
+                    final_data[instance][channel][int(timepoint/self.window_space)]=value
         return final_data
 
 
