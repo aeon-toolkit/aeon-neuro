f051bffdc1a81988b131d500e9ffbe7a8603854e
Futuer <57895730+futuer-szd@users.noreply.github.com>
2024-06-14 10:42:52 +0100

    [DOC,MNT,BUG] Documentation and testing for downsample.py (#45)
    * 1.Fixes a bug in downsample.py when
    n_timepoints / new_ratio is an integer
    2.complete a testing for downsample.py
    in transformations/tests/test_downsample.py
    3.write documentation for transformations/downsample.py
    
    * 1.Fixes a bug in downsample.py
    when n_timepoints / new_ratio is an integer
    2.complete a testing for downsample.py
    in transformations/tests/test_downsample.py
    3.write documentation for transformations/downsample.py
    
    * modify the variable names
    both in the code and comments
    
    ---------
    
    Co-authored-by: futuer <barryszd@outlook.com>

--- aeon_neuro/transformations/downsample.py
+++ aeon_neuro/transformations/downsample.py
@@ -1,13 +1,32 @@
+"""Downsample series by frequency."""
+
 import numpy as np
 
 
 def downsample_series(X, sfreq, target_sample_rate):
+    """Downsample a time series.
+
+    Parameters
+    ----------
+    X : a numpy array of shape = [n_cases, n_channels, n_timepoints]
+        The input time series data to be downsampled.
+    sfreq : int
+        The original sampling frequency of the time series data.
+    target_sample_rate : int
+        The target sampling frequency after downsampling.
+
+    Returns
+    -------
+    downsampled : a numpy array of
+        shape = [n_cases, n_channels, updated_timepoints]
+        The downsampled time series data.
+    """
     new_ratio = int(sfreq / target_sample_rate)
-    n_instances, n_dimensions, n_timepoints = np.shape(X)
-    updated_timepoints = int(n_timepoints / new_ratio)
-    downsampled_data = np.zeros((n_instances, n_dimensions, updated_timepoints + 1))
-    for i in range(n_instances):
-        for j in range(n_dimensions):
+    n_cases, n_channels, n_timepoints = np.shape(X)
+    updated_timepoints = int(np.ceil(n_timepoints / new_ratio))
+    downsampled_data = np.zeros((n_cases, n_channels, updated_timepoints))
+    for i in range(n_cases):
+        for j in range(n_channels):
             updated_index = 0
             for k in range(0, n_timepoints, new_ratio):
                 downsampled_data[i][j][updated_index] = X[i][j][k]
