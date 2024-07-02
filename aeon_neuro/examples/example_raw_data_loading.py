7f6979514a6b112ce4a47e193b4a459b19bfc0a4
AidenRushbrooke <72034940+AidenRushbrooke@users.noreply.github.com>
2024-07-02 19:21:20 +0100

    Fix pre-commit issues

--- aeon_neuro/examples/example_raw_data_loading.py
+++ aeon_neuro/examples/example_raw_data_loading.py
@@ -1,23 +1,19 @@
 import mne
-import numpy as np
-from scipy.linalg import logm
-import math
+
 
 def load_example_dataset():
-    #Returns two instances of raw EEG data, the first being resting state eyes closed, the second resting state eyes open
-    instances=[]
+    # Returns two instances of raw EEG data, the first being resting state eyes closed, the second resting state eyes open
+    instances = []
     instances.append(
         mne.io.read_raw_brainvision(
             "./aeon_neuro/data/example_dataset/sub-01/ses-01/eeg/sub-01_ses-01_task-EC_run-01_eeg.vhdr",
-            preload=True
+            preload=True,
         ).get_data()
     )
     instances.append(
         mne.io.read_raw_brainvision(
-            "./aeon_neuro/data/example_dataset/sub-01/ses-01/eeg/sub-01_ses-01_task-EO_run-01_eeg.vhdr"
-            ,preload=True
+            "./aeon_neuro/data/example_dataset/sub-01/ses-01/eeg/sub-01_ses-01_task-EO_run-01_eeg.vhdr",
+            preload=True,
         ).get_data()
     )
     return instances
-    
-
