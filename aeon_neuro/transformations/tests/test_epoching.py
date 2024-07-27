2e435e638060d47dd23decd27411fe085e87a0f1
Gabriel Riegner <gabriel.riegner@gmail.com>
2024-07-26 21:55:10 -0700

    implement class that inherits from BaseSeriesTranformer

--- aeon_neuro/transformations/tests/test_epoching.py
+++ aeon_neuro/transformations/tests/test_epoching.py
@@ -11,8 +11,8 @@ n_timepoints_per_epoch = int((epoch_size / 1000) * sfreq)
 n_epochs = int(n_timepoints / n_timepoints_per_epoch)
 
 # test series
-series = np.arange(n_channels * n_timepoints).reshape(n_channels, -1)
-series_expected = np.array(
+X_series = np.arange(n_channels * n_timepoints).reshape(n_channels, -1)
+X_series_expected = np.array(
     [
         [[0, 1, 2], [12, 13, 14]],
         [[3, 4, 5], [15, 16, 17]],
@@ -22,26 +22,34 @@ series_expected = np.array(
 )
 
 # test dataset
-X = np.array([series, series + 1, series + 2])
-X_expected = np.vstack([series_expected, series_expected + 1, series_expected + 2])
+X_collection = np.array([X_series, X_series + 1, X_series + 2])
+X_collection_expected = np.vstack(
+    [X_series_expected, X_series_expected + 1, X_series_expected + 2]
+)
 y = np.arange(n_cases)
 y_expected = np.repeat(y, n_epochs)
 
 
 def test_epoch_series_by_percentage():
     """Test series-to-collection transformation."""
-    series_transformed = epoching.epoch_series_by_percentage(series, percent)
-    np.testing.assert_array_equal(series_expected, series_transformed)
+    transformer = epoching.EpochSeriesTransformer(percent=percent)
+    X_series_transformed = transformer.fit_transform(X_series)
+    assert isinstance(X_series_transformed, np.ndarray)
+    np.testing.assert_array_equal(X_series_expected, X_series_transformed)
 
 
 def test_epoch_series_by_time():
     """Test series-to-collection transformation."""
-    series_transformed = epoching.epoch_series_by_time(series, sfreq, epoch_size)
-    np.testing.assert_array_equal(series_expected, series_transformed)
+    transformer = epoching.EpochSeriesTransformer(sfreq=sfreq, epoch_size=epoch_size)
+    X_series_transformed = transformer.fit_transform(X_series)
+    assert isinstance(X_series_transformed, np.ndarray)
+    np.testing.assert_array_equal(X_series_expected, X_series_transformed)
 
 
 def test_epoch_dataset():
     """Test collection-to-collection transformation."""
-    X_transformed, y_transformed = epoching.epoch_dataset(X, y, sfreq, epoch_size)
-    np.testing.assert_array_equal(X_transformed, X_expected)
+    X_transformed, y_transformed = epoching.epoch_dataset(
+        X_collection, y, sfreq, epoch_size
+    )
+    np.testing.assert_array_equal(X_transformed, X_collection_expected)
     np.testing.assert_array_equal(y_transformed, y_expected)
