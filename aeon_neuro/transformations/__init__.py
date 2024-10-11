b66ce6d94968055ddf44c291330254428d61ebc9
AidenRushbrooke <72034940+AidenRushbrooke@users.noreply.github.com>
2024-10-11 01:42:18 +0100

    move into channel selection directory

--- aeon_neuro/transformations/__init__.py
+++ aeon_neuro/transformations/__init__.py
@@ -4,11 +4,9 @@ __all__ = [
     "BandPowerSeriesTransformer",
     "EpochSeriesTransformer",
     "DownsampleCollectionTransformer",
-    "UMAPTransformer",
 ]
 
 
 from aeon_neuro.transformations._bandpower import BandPowerSeriesTransformer
 from aeon_neuro.transformations._downsample import DownsampleCollectionTransformer
 from aeon_neuro.transformations._epoching import EpochSeriesTransformer
-from aeon_neuro.transformations._umaptransformer import UMAPTransformer
---
+++ aeon_neuro/transformations/collections/channel_selection/__init__.py
@@ -0,0 +1,8 @@
+"""EEG related channel selection."""
+
+__all__ = [
+    "UMAPTransformer",
+]
+
+
+from aeon_neuro.transformations.collections.channel_selection._umaptransformer import UMAPTransformer
