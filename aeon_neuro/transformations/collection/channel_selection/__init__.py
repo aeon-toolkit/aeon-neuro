effc961dbdc3320ea30e6ab56833873537001f02
AidenRushbrooke <72034940+AidenRushbrooke@users.noreply.github.com>
2024-10-18 11:06:29 +0100

    UMAP Transformer (#96)
    * UMAP transformer
    
    * move into channel selection directory
    
    * rename
    
    * Added UMAP as dependency
    
    * update docstring
    
    * add newline
    
    * Update __init__.py

---
+++ aeon_neuro/transformations/collection/channel_selection/__init__.py
@@ -0,0 +1,10 @@
+"""EEG related channel selection."""
+
+__all__ = [
+    "UMAPTransformer",
+]
+
+
+from aeon_neuro.transformations.collection.channel_selection._umaptransformer import (
+    UMAPTransformer,
+)
